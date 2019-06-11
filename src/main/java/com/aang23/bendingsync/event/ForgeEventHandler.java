178e3abbe65235866d6397a97bac65d250868bc5
Aang23 <qwerty15@gmx.fr>
2019-06-11 13:53:59 +0200

    Temp fix

--- src/main/java/com/aang23/bendingsync/event/ForgeEventHandler.java
+++ src/main/java/com/aang23/bendingsync/event/ForgeEventHandler.java
@@ -3,21 +3,21 @@ package com.aang23.bendingsync.event;
 import com.aang23.bendingsync.BendingSync;
 import com.crowsofwar.avatar.common.event.BendingEvent;
 
+import org.spongepowered.api.entity.living.player.Player;
+
 import me.lucko.luckperms.api.Node;
 import me.lucko.luckperms.api.User;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import io.github.nucleuspowered.nucleus.api.NucleusAPI;
 
 public class ForgeEventHandler {
-    private static Node preventBendingNode = BendingSync.LUCKPERMS_API.getNodeFactory()
-            .newBuilder("bendingsync.bending.disable").build();
-
     @SubscribeEvent
     public static void onBending(BendingEvent event) {
-        // Using LuckPerms' API... I hope contexts gets updated faster here
-        User permUser = BendingSync.LUCKPERMS_API.getUser(event.getEntity().getUniqueID());
-        if (permUser.hasPermission(preventBendingNode).asBoolean())
-            event.setCanceled(true);
-
+        if (event.getEntity() instanceof EntityPlayer) {
+            Player spongePlayer = (Player) event.getEntity();
+            // TODO use luckperms !
+            if (spongePlayer.hasPermission("bendingsync.bending.disable"))
+                event.setCanceled(true);
+        }
     }
 }
