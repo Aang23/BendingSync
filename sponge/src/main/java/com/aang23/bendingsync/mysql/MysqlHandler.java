a20b2c2ed9328b0b672f3c7bb45a34e10657267d
Aang23 <qwerty15@gmx.fr>
2019-07-23 18:33:45 +0200

    Since Sql2o didn't want to work, go back to the old fetching method

--- sponge/src/main/java/com/aang23/bendingsync/mysql/MysqlHandler.java
+++ sponge/src/main/java/com/aang23/bendingsync/mysql/MysqlHandler.java
@@ -1,11 +1,18 @@
 package com.aang23.bendingsync.mysql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.UUID;
 import java.util.Map.Entry;
 
 import com.aang23.bendingsync.BendingSync;
+import com.aang23.bendingsync.ConfigManager;
 import com.aang23.bendingsync.storage.BendingDataStorage;
 import com.aang23.bendingsync.storage.CommonDataStorage;
 import com.aang23.bendingsync.storage.DSSDataStorage;
@@ -14,6 +21,9 @@ import com.aang23.bendingsync.storage.InventoryDataStorage;
 import org.sql2o.ResultSetHandlerFactory;
 
 public class MysqlHandler {
+    private static Connection connection;
+    private static Properties properties;
+
     public static void setupDatabase() {
         BendingSync.MYSQL.open().createQuery(
                 "CREATE TABLE IF NOT EXISTS players_data (uuid VARCHAR(100), bending TEXT(10000), dss TEXT(10000), inventory TEXT(10000), PRIMARY KEY (`uuid`))")
@@ -53,18 +63,58 @@ public class MysqlHandler {
 
     public static boolean doesPlayerExists(String uuid) {
         return BendingSync.MYSQL.open().createQuery("SELECT EXISTS(SELECT * FROM players_data WHERE uuid=:uuid)")
-                .addParameter("uuid", uuid)
-                .executeScalar(Boolean.class);
+                .addParameter("uuid", uuid).executeScalar(Boolean.class);
     }
 
     private static String getContentForUuidOf(String uuid, String column) {
-        // @formatter:off
-        String result = BendingSync.MYSQL.open().createQuery("SELECT * FROM players_data WHERE uuid=:uuid")
-                                .addParameter("column", column)
-                                .addParameter("uuid", uuid)
-                                .executeAndFetch(String.class).toString();
-        // @formatter:on
+        String result = null;
+        String sql = "SELECT " + column + " FROM players_data WHERE bender_uuid='" + uuid + "';";
+        try {
+            PreparedStatement statement = connectStandard().prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            rs.next();
+            result = rs.getString(column);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            disconnectStandard();
+        }
         System.out.println(result.toString());
         return result;
+    }
+
+    public static Connection connectStandard() {
+        String DATABASE_URL = "jdbc:mysql://" + ConfigManager.address + ":" + ConfigManager.port + "/"
+                + ConfigManager.database
+                + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(DATABASE_URL, getProperties());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void disconnectStandard() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            properties.setProperty("user", ConfigManager.username);
+            properties.setProperty("password", ConfigManager.password);
+            properties.setProperty("MaxPooledStatements", "250");
+        }
+        return properties;
     }
 }
